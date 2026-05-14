7b1150d03a5f9ab545f4d908a747ac008166e287
jineesha-gupta <guptajineesha@gmail.com>
2026-05-15 00:35:26 +0530

    fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,10 @@ const {
 
 /* MAIL SETUP */
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
